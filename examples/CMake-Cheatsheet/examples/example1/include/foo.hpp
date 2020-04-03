faafe924f532484939c000f5c2b545b44b7b74ed
Ali <nehrani@gmail.com>
2020-04-03 08:52:29 +0300

    updating tutorial

---
+++ examples/CMake-Cheatsheet/examples/example1/include/foo.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class foo{
+    public:
+          static double hello( double ); 
+};
+
+class foo2{
+    private:
+       void pname(const char *);
+    public:
+    double divisor (double, double);
+};
